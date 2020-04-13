5727f027e54ca4b5716906ee3137cbacc9590d0a
daucf23 <davidalmeida2@knights.ucf.edu>
2020-04-13 13:03:10 -0400

    minor changes

--- src/javaApp/GuiApp1.java
+++ src/javaApp/GuiApp1.java
@@ -8,7 +8,6 @@ import javax.swing.JTextField;
 import javax.swing.JComboBox;
 import javax.swing.JButton;
 import javax.swing.JLabel;
-import javax.swing.JList;
 import java.awt.BorderLayout;
 import java.awt.event.ActionListener;
 import java.io.IOException;
@@ -28,144 +27,117 @@ public class GuiApp1
     // operation == 1 means writing to file..... operation == 0 means read file;
     // operation == -1 means remove from file;
 	
-	public static void main(String[] args) 
-	{
+	public static void main(final String[] args) {
 		new GuiApp1();
 	}
 
-	
-	public GuiApp1()
-	{
-		JFrame guiFrame = new JFrame();
-		
-		//make sure the program exits when the frame closes
+	public GuiApp1() {
+		final JFrame guiFrame = new JFrame();
+
+		// make sure the program exits when the frame closes
 		guiFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		guiFrame.setTitle("Shiny Pokemon");
-		guiFrame.setSize(300,300);
-		
-		//This will center the JFrame in the middle of the screen
+		guiFrame.setSize(300, 300);
+
+		// This will center the JFrame in the middle of the screen
 		guiFrame.setLocationRelativeTo(null);
-		
-		//Options for the JComboBox
-		String[] menuOptions = { "Create List","Add Shiny", "List Shinies", "Remove Shinies"};
-		
-		//Options for the JList
-		//String[] shinyList = {"Larvitar", "Grookey", "Grookey", "Scorbunny"
-		//		, "Scorbunny", "Sobble", "Eevee", "Umbreon", "Onyx"};
-		
-		//The first JPanel contains a JLabel and JCombobox
+
+		// Options for the JComboBox
+		final String[] menuOptions = { "Create List", "Add Shiny", "List Shinies", "Remove Shinies" };
+
+		// The first JPanel contains a JLabel and JCombobox
 		final JPanel comboPanel = new JPanel();
-		JLabel comboLbl = new JLabel("Make a Selection:");
-		JComboBox dropMenu = new JComboBox(menuOptions);
+		final JLabel comboLbl = new JLabel("Make a Selection:");
+		final JComboBox dropMenu = new JComboBox(menuOptions);
 		comboPanel.add(comboLbl);
 		comboPanel.add(dropMenu);
-		
-		
-		//Create the third JPanel. Add a JLabel and JList and
-		//make use the JPanel is not visible.
+
+		// Create the third JPanel. Add a JLabel and JList and
+		// make use the JPanel is not visible.
 		final JPanel listPanel = new JPanel();
 		listPanel.setVisible(false);
-		JLabel listLbl = new JLabel("Shinies:");
-		//JList shiniesList = new JList(shinyList);
-		//shiniesList.setLayoutOrientation(JList.HORIZONTAL_WRAP);
+		final JLabel listLbl = new JLabel("Shinies:");
 		listPanel.add(listLbl);
-		//listPanel.add(shiniesList);
-		JButton selectBut = new JButton( "Select");
-		
-		//The ActionListener class is used to handle the
-		//event that happens when the user clicks the button.
-		//As there is not a lot that needs to happen we can
-		//define an anonymous inner class to make the code simpler.
-		selectBut.addActionListener(new ActionListener()
-		{
-			
+		final JButton selectBut = new JButton("Select");
+
+		// The ActionListener class is used to handle the
+		// event that happens when the user clicks the button.
+		// As there is not a lot that needs to happen we can
+		// define an anonymous inner class to make the code simpler.
+		selectBut.addActionListener(new ActionListener() {
+
 			@Override
-			public void actionPerformed(ActionEvent event)
-			{
-				//When the fruit of veg button is pressed
-				//the setVisible value of the listPanel and
-				//comboPanel is switched from true to
-				//value or vice versa.
-				//System.out.println((String)dropMenu.getSelectedItem());
-				if("Create List" == (String)dropMenu.getSelectedItem())
-				{
+			public void actionPerformed(final ActionEvent event) {
+				// When the fruit of veg button is pressed
+				// the setVisible value of the listPanel and
+				// comboPanel is switched from true to
+				// value or vice versa.
+				// System.out.println((String)dropMenu.getSelectedItem());
+				if ("Create List" == (String) dropMenu.getSelectedItem()) {
 					try {
 						fileReadWrite.createFile();
-					} catch (IOException e) {
+					} catch (final IOException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
-				}
-				else if("Add Shiny" == (String)dropMenu.getSelectedItem())
-				{
+				} else if ("Add Shiny" == (String) dropMenu.getSelectedItem()) {
 					operation = 1;
 					create();
-				}
-				else if("List Shinies" == (String)dropMenu.getSelectedItem())
-				{
+				} else if ("List Shinies" == (String) dropMenu.getSelectedItem()) {
 					try {
 						display();
 						System.out.println("Display method reached");
-					} catch (IOException e) {
+					} catch (final IOException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
-				}
-				else if("Remove Shinies" == (String)dropMenu.getSelectedItem()) 
-				{
+				} else if ("Remove Shinies" == (String) dropMenu.getSelectedItem()) {
 					remove();
-				}
-				else 
-				{
+				} else {
 					System.exit(0);
 				}
 			}
 		});
-		//The JFrame uses the BorderLayout layout manager.
-		//Put the two JPanels and JButton in different areas.
+		// The JFrame uses the BorderLayout layout manager.
+		// Put the two JPanels and JButton in different areas.
 		guiFrame.add(comboPanel, BorderLayout.NORTH);
 		guiFrame.add(listPanel, BorderLayout.CENTER);
-		guiFrame.add(selectBut,BorderLayout.SOUTH);
-		//make sure the JFrame is visible
+		guiFrame.add(selectBut, BorderLayout.SOUTH);
+		// make sure the JFrame is visible
 		guiFrame.setVisible(true);
 	}
-	
-	public static void create() 
-	{
-		
-		
-		//System.out.println("hello world");
-		CreateEntrybox me = new CreateEntrybox();
-			
-			//me.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			me.setVisible(true);
-			me.setLocationRelativeTo(null);
-			me.setSize(300, 300);
-			me.setTitle("Enter new Shiny");
-			
-			
+
+	public static void create() {
+
+		// System.out.println("hello world");
+		final CreateEntrybox me = new CreateEntrybox();
+
+		// me.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		me.setVisible(true);
+		me.setLocationRelativeTo(null);
+		me.setSize(300, 300);
+		me.setTitle("Enter new Shiny");
+
 	}
-	
-	public static void display() throws IOException 
-	{
+
+	public static void display() throws IOException {
 		operation = 0;
 		fileReadWrite.readFile();
-		//System.out.println("hello world");
-		CreateDisplaybox me = new CreateDisplaybox();
-			
-			//me.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			me.setVisible(true);
-			me.setLocationRelativeTo(null);
-			me.setSize(300, 300);
-			me.setTitle("List of Shiny Pokemon");
-			
+		// System.out.println("hello world");
+		final CreateDisplaybox me = new CreateDisplaybox();
+
+		// me.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		me.setVisible(true);
+		me.setLocationRelativeTo(null);
+		me.setSize(300, 300);
+		me.setTitle("List of Shiny Pokemon");
+
 	}
-	
-	public static void remove() 
-	{
+
+	public static void remove() {
 		operation = -1;
-		//System.out.println("hello world");
-		CreateEntrybox me = new CreateEntrybox();
+		// System.out.println("hello world");
+		final CreateEntrybox me = new CreateEntrybox();
 			
 			//me.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			me.setVisible(true);
